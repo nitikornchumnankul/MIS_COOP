4d9e83622b3399e6a0b9b7cf42360b3cdfa3310e
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-07-21 00:22:51 +0700

    create folder file-management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { fuseConfig } from 'app/fuse-config';
 import { AppComponent } from 'app/app.component';
 import { LayoutModule } from 'app/layout/layout.module';
 import { SampleModule } from 'app/main/sample/sample.module';
+import { FileManagerComponent } from './app/main/apps/file-manager/file-manager.component';
 
 const appRoutes: Routes = [
     {
@@ -28,7 +29,8 @@ const appRoutes: Routes = [
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        FileManagerComponent
     ],
     imports     : [
         BrowserModule,
