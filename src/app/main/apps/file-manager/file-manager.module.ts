35ed26e4ba48d687454f8697b972a05fce5ae421
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-07-21 01:44:08 +0700

    Create Folder file manager

--- src/app/main/apps/file-manager/file-manager.module.ts
+++ src/app/main/apps/file-manager/file-manager.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileListComponent } from './file-list/file-list.component';
+import { DetailsComponent } from './sidebars/details/details.component';
+import { MainComponent } from './sidebars/main/main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FileListComponent, DetailsComponent, MainComponent],
   imports: [
     CommonModule
   ]
